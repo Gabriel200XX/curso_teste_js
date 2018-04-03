1e1efce1c37d88013b8cc089069adfcd90495580
Evandro de Oliveira <evandrojlle@gmail.com>
2018-04-03 09:46:52 -0300

    Ajustado o karma para o projeto

--- jasmine-node/spec/exemplo/exemploSpec.js
+++ jasmine-node/spec/exemplo/exemploSpec.js
@@ -2,4 +2,7 @@ describe("Suite de teste de exemplo com NodeJS", function() {
   it("Deve demonstrar o uso do Jasmine com o NodeJS", function() {
     expect(true).toEqual(true);
   });
+  it("Deve demonstrar o uso do Karma em modo de escuta", function() {
+    expect(1).toEqual(1);
+  });
 });
